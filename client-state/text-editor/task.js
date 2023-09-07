34b5c07a17e74d98ffee48122f71a4f70b9e8f3d
Zhanna <zhuzha.jukova@yandex.ru>
2023-09-07 20:07:37 +0700

    fixed solution task 3

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,12 +1,12 @@
-let editor = document.getElementById('editor');
-let buttonClear = document.getElementById('button');
-buttonClear.addEventListener('click', () => {
-	localStorage.removeItem('text');
-	editor.value = '';
-})
-editor.addEventListener('input', (e) => {
-	localStorage.setItem('text', editor.value)
-})
-window.onload = function() {
-	editor.value = localStorage.getItem('text')
-}
+'use strict';
+
+const editor = document.getElementById('editor');
+const button = document.getElementById('clear');
+
+const key = 'editorText';
+
+button.addEventListener('click', () => editor.value = '');
+
+window.addEventListener('unload', () => localStorage[key] = editor.value);
+
+editor.value = localStorage[key];
