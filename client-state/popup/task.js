d67fb6e6037aaac264bb9212d9174de503048806
Zhanna <zhuzha.jukova@yandex.ru>
2023-09-07 18:22:06 +0700

    Fixed task2

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,17 +1,18 @@
-let modal = document.getElementById('subscribe-modal');
-modal.classList.add('modal_active');
-let modalClose = document.querySelector('.modal__close');
-window.onload = function() {
-	if (document.cookie.indexOf("name") == 0) {
-		modal.classList.remove('modal_active')
-	}
-	modalClose.addEventListener('click', event => {
-		event.preventDefault();
-		let target = event.target;
-		target.closest('.modal').classList.remove('modal_active');
-		let today = new Date();
-		let tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
-		let dayTomorrow = tomorrow;
-		document.cookie = `name=close; ${dayTomorrow}`;
-	})
+const modalWindow = document.getElementById("subscribe-modal");
+const modalClose = document.querySelector(".modal__close");
+
+const getCookie = (name) => {
+   const value = ";" + document.cookie;
+   let parts = value.split(";" + name + "=");
+   if (parts.length === 2) {
+      return parts.pop().split(";").shift();
+   }
+}
+
+modalClose.addEventListener("click", () => {
+   modalWindow.classList.remove("modal_active");
+});
+
+if (getCookie("modal") !== "close") {
+   modalWindow.classList.add("modal_active");
 }
